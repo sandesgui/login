bb5f10b3c50603fca9d363e22b90f22be2038298
sandesgui <sandesguilherme06@gmail.com>
2023-09-21 16:28:24 -0300

    pagina login alterada

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <body>
     <!--início do formulário-->
     <form action="login.php" class="container card p-5 m-5" method="post">
-        <h1 class="text-center">Faça seu login abaixo:</h1>
+        <h1 class="text-center">login:</h1>
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" name="nome" class="form-control" id="nome">
